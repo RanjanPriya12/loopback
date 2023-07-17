e86b73d11bb41343a59fc7ef7d07a98b5f192182
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-07-17 23:36:49 +0530

    commit

--- project/common/models/blog.js
+++ project/common/models/blog.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Blog) {
-
+    console.log("first")
+//   Blog.addBlog = async function (body){
+//     const self = this;
+//     const BlogModel = self.getDataSource().connector.collection(Blog.modelName);
+//     try {
+//         const blog = await BlogModel.insert({message:"Blog Created Succesufully.",blog:{title:body.title,description:body.description,image:body.image}})
+//     } catch (error) {
+        
+//     }
+//   }
+//   Test.createOrder = function(amount, callback) {
+//     // ...
+//     Test.app.models.Log.create(/*...*/)
+//     // ...
+// };
 };
